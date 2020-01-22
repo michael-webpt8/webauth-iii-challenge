aea05649e106fa35a3efa70d7f0508f110c28870
Michael Williams <manicdesigner@gmail.com>
2020-01-22 17:18:15 -0500

    add login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,7 +7,7 @@ const secrets = require('../config/secrets');
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
   if (!req.body.username) {
     res.status(400).json({ message: 'username required' });
   }
@@ -31,6 +31,37 @@ router.post('/register', async (req, res) => {
     }
   } catch (err) {
     console.log(err);
+    next(err);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await userDb.findBy({ username }).first();
+
+    const passwordCheck = await bcrypt.compare(password, user.password);
+
+    if (user && passwordCheck) {
+      const token = jwt.sign(
+        {
+          subject: user.id,
+          username: user.username,
+          department: user.department,
+        },
+        secrets.jwt,
+        {
+          expiresIn: '7d',
+        }
+      );
+
+      res
+        .status(200)
+        .json({ message: `Welcome ${user.username}`, token: token });
+    } else {
+      res.status(401).json({ message: 'invalid credentials' });
+    }
+  } catch (err) {
     next(err);
   }
 });
