0d9da603a26b3e5c18e08d6aa9a68dc8921114d2
Michael Williams <manicdesigner@gmail.com>
2020-01-21 20:50:25 -0500

    built out functions find, findBy, findById, add for users-model

---
+++ users/users-model.js
@@ -0,0 +1,33 @@
+const db = require('../data/db-config');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add,
+};
+
+function find() {
+  return db('users').select('id', 'username');
+}
+
+function findBy(filter) {
+  return db('users')
+    .where(filter)
+    .select('id', 'username', 'password');
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .select('id', 'username')
+    .first();
+}
+
+async function add(user) {
+  user.password = await bcrypt.hash(user.password, 14);
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
