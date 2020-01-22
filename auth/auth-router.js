02e8cf467065e9134ebd5953063523b657e9e410
Michael Williams <manicdesigner@gmail.com>
2020-01-21 22:51:04 -0500

    add router.post for auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,4 +7,28 @@ const secrets = require('../config/secrets');
 
 const router = express.Router();
 
+router.post('/register', async (req, res) => {
+  if (!req.body.username) {
+    res.status(400).json({ message: 'username required' });
+  }
+  if (!req.body.password) {
+    res.status(400).json({ message: 'password required' });
+  }
+  const newUser = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  try {
+    const user = await userDb.add(newUser);
+    if (user) {
+      res.status(201).json(user);
+    } else {
+      res.status(404).json({ message: 'unable to create user.' });
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
 module.exports = router;
